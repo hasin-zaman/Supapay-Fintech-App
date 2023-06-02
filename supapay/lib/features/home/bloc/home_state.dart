95796d3579b6b0c7835ffebe10612848f0e6bdc7
Owais <owaisalisyed.8@gmail.com>
2023-06-03 00:51:48 +0500

    BloC Tests Added

--- supapay/lib/features/home/bloc/home_state.dart
+++ supapay/lib/features/home/bloc/home_state.dart
@@ -1,7 +1,10 @@
 part of 'home_bloc.dart';
 
 @immutable
-abstract class HomeState {}
+abstract class HomeState extends Equatable {
+  @override
+  List<Object> get props => [];
+}
 
 abstract class HomeActionState extends HomeState {}
 
